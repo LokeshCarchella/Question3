5f4988e1d0d4c1ba6c32c2ee3ffdacf5c951a0f5
Lokesh Carchella <ContactLokesh@protonmail.com>
2025-01-09 12:09:52 +0530

    Create test.java
    Q3

---
+++ test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.*;
+
+public class Addition{
+  public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+    int a = 5;
+    int b = 10;
+    int c = a+b;
+    System.out.println("Addition: "+ c);
+  }
+  
+}
